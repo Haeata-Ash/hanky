12bcde710ba40edf8f65ddfaf70dcefaf6c23450
Haeata <haeata.ash@pm.me>
2026-06-30 11:13:10 +0200

    Refactored functions from Hanky class to private module used to interact with anki collection. Decouples anki functionality from the public hanky api

---
+++ src/hanky/anki_utils.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+from typing import Any
+from anki.collection import Collection
+from anki.notes import NoteFieldsCheckResult
+
+
+def add_media(
+    col: Collection,
+    data: Any,
+    media_fname: str,
+) -> str:
+    """Add binary data to the anki collection.
+
+    Args:
+        data: the binary media data
+        media_fname: The filename including the extension
+
+    Returns:
+        The media filename after adding it to anki collection
+    """
+    desired_name = media_fname
+
+    # write media to anki database
+    actual_name = col.media.write_data(
+        desired_name,
+        data,
+    )
+
+    return actual_name
+
+
+def backup_collection(col: Collection, backup_folder: str):
+    """Backups the anki collection to a folder"""
+    folder_path = Path(backup_folder)
+    folder_path.mkdir(parents=True, exist_ok=True)
+    if not col.create_backup(
+        backup_folder=backup_folder,
+        force=True,
+        wait_for_completion=True,
+    ):
+        raise RuntimeError("Unable to create backup of anki collection.")
+
+
+def add_card(
+    col: Collection,
+    deck_name: str,
+    model_name: str,
+    allow_duplicates=False,
+    **fields,
+) -> bool:
+    """Adds a card of a given model type to a deck.
+
+    Args:
+        deck_name: the full destination deck name as a seen in anki.
+        model_name: the name of the flash card model as seen in anki.
+
+    Returns:
+        A bool, true if the card was successfully added, false otherwise
+
+    Raises:
+        ValueError: The deck or model don't exist
+        KeyError: The card does not have the required fields for the model
+    """
+    model = col.models.by_name(model_name)
+    if model is None:
+        raise ValueError(
+            f"Model '{model_name}' does not exist in your anki collection. Ensure it has been added before using it with hanky."
+        )
+    deck_id = col.decks.id(deck_name, create=False)
+    if deck_id is None:
+        raise ValueError(
+            f"Deck '{deck_name}' does not exist in your anki collection. Ensure it has been created before using it with hanky."
+        )
+
+    expected_fields = col.models.field_names(model)
+    for k in expected_fields:
+        if k not in fields:
+            raise KeyError(f"Expected field '{k}' is missing. {fields}")
+
+    new_card = col.new_note(model)
+
+    for k, v in fields.items():
+        new_card[k] = str(v).strip()
+
+    # use anki builtin duplicate detection to check for duplicates
+    if not allow_duplicates:
+        card_state = new_card.duplicate_or_empty()
+        if card_state == NoteFieldsCheckResult.DUPLICATE:
+            return False
+
+    col.add_note(new_card, deck_id)
+    return True
+
+
+def add_deck(col: Collection, deck_name: str):
+    """Adds a deck to anki. If the deck already exists nothing will happen
+
+    Args:
+        deck_name: the full name of the deck to be added
+    """
+    col.decks.id(deck_name)
--- src/hanky/hanky.py
+++ src/hanky/hanky.py
@@ -1,7 +1,6 @@
 from contextlib import contextmanager
 from pathlib import Path
 from typing import (
-    Any,
     Callable,
     Dict,
     Iterable,
@@ -14,14 +13,13 @@ from typing import (
 
 from anki.collection import Collection
 
+from hanky.anki_utils import add_card, add_deck, add_media, backup_collection
 from hanky.processors import CardProcessingException, ModelProcessor
 from hanky.cli import make_parser
 from hanky.config import Config
 from hanky.fs import DEFAULT_LOADERS, Loader, has_handle, make_file_loader
 from hanky.media import CardMedia
 from hanky.report import CardError, LoadReport
-
-from anki.notes import NoteFieldsCheckResult
 
 _DEFAULT_CONFIG_PATH = Path("~/.config/hanky/hanky.toml").expanduser()
 
@@ -138,75 +136,6 @@ class Hanky:
         finally:
             if was_opened_here:
                 self._close_collection()
-
-    def backup_collection(self, col: Collection, backup_folder: str):
-        """Backups the anki collection to the configured backup"""
-        folder_path = Path(backup_folder)
-        folder_path.mkdir(parents=True, exist_ok=True)
-        if not col.create_backup(
-            backup_folder=backup_folder,
-            force=True,
-            wait_for_completion=True,
-        ):
-            raise RuntimeError("Unable to create backup of anki collection.")
-
-    def add_card(
-        self,
-        col: Collection,
-        deck_name: str,
-        model_name: str,
-        **fields,
-    ) -> bool:
-        """Adds a card of a given model type to a deck.
-
-        Args:
-            deck_name: the full destination deck name as a seen in anki.
-            model_name: the name of the flash card model as seen in anki.
-
-        Returns:
-            A bool, true if the card was successfully added, false otherwise
-
-        Raises:
-            ValueError: The deck or model don't exist
-            KeyError: The card does not have the required fields for the model
-        """
-        model = col.models.by_name(model_name)
-        if model is None:
-            raise ValueError(
-                f"Model '{model_name}' does not exist in your anki collection. Ensure it has been added before using it with hanky."
-            )
-        deck_id = col.decks.id(deck_name, create=False)
-        if deck_id is None:
-            raise ValueError(
-                f"Deck '{deck_name}' does not exist in your anki collection. Ensure it has been created before using it with hanky."
-            )
-
-        expected_fields = col.models.field_names(model)
-        for k in expected_fields:
-            if k not in fields:
-                raise KeyError(f"Expected field '{k}' is missing. {fields}")
-
-        new_card = col.new_note(model)
-
-        for k, v in fields.items():
-            new_card[k] = str(v).strip()
-
-        # use anki builtin duplicate detection to check for duplicates
-        if not self.config.ALLOW_DUPLICATES:
-            card_state = new_card.duplicate_or_empty()
-            if card_state == NoteFieldsCheckResult.DUPLICATE:
-                return False
-
-        col.add_note(new_card, deck_id)
-        return True
-
-    def add_deck(self, col: Collection, deck_name: str):
-        """Adds a deck to anki. If the deck already exists nothing will happen
-
-        Args:
-            deck_name: the full name of the deck to be added
-        """
-        col.decks.id(deck_name)
 
     def register_loader(
         self, file_ext: str, loader: Loader, is_text=True, **fopen_kwargs
@@ -356,7 +285,7 @@ class Hanky:
                     f"Model '{model_name}' does not exist in your anki collection. Ensure it has been added before using it with hanky."
                 )
 
-            self.add_deck(col, deck_name)
+            add_deck(col, deck_name)
 
             added = 0
             skipped = 0
@@ -377,10 +306,16 @@ class Hanky:
                     # TODO: we are leaving the media in the db even if the card
                     # isn't added
                     for m in media:
-                        actual_fname = self.add_media(col, m.data, m.desired_name)
+                        actual_fname = add_media(col, m.data, m.desired_name)
                         m.replace_temp_refs(actual_fname, card)
 
-                    if self.add_card(col, deck_name, model_name, **card):
+                    if add_card(
+                        col,
+                        deck_name,
+                        model_name,
+                        allow_duplicates=self.config.ALLOW_DUPLICATES,
+                        **card,
+                    ):
                         added += 1
                     else:
                         skipped += 1
@@ -429,31 +364,6 @@ class Hanky:
             source, model_name, deck_name, fail_fast=fail_fast, **model_args
         )
         return report.with_source(str(path))
-
-    def add_media(
-        self,
-        col: Collection,
-        data: Any,
-        media_fname: str,
-    ) -> str:
-        """Add binary data to the anki collection.
-
-        Args:
-            data: the binary media data
-            media_fname: The filename including the extension
-
-        Returns:
-            The media filename after adding it to anki collection
-        """
-        desired_name = media_fname
-
-        # write media to anki database
-        actual_name = col.media.write_data(
-            desired_name,
-            data,
-        )
-
-        return actual_name
 
     def load_dir(
         self,
@@ -545,7 +455,7 @@ class Hanky:
 
         """
         with self.session() as col:
-            self.backup_collection(col, self.config.BACKUP_FOLDER)
+            backup_collection(col, self.config.BACKUP_FOLDER)
 
         _run_app(self)
 
