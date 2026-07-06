d188639e178ced215b35279a3b3d656cd93561d9
Haeata <haeata.ash@pm.me>
2026-07-06 12:26:55 +0200

    Remove deprecated classes, methods, args for 0.3.0

--- src/hanky/__init__.py
+++ src/hanky/__init__.py
@@ -1,5 +1,4 @@
 from .hanky import HankyPipeline as HankyPipeline
-from .hanky import Hanky as Hanky  # deprecated
 from .media import CardMedia as CardMedia
 from .report import CardError as CardError
 from .report import LoadReport as LoadReport
