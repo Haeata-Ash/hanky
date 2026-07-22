c4f0018da1d1d728856e1fef0c8cd38029e4dee0
Haeata <haeata.ash@pm.me>
2026-07-22 22:18:42 +0200

    Export the config error type

--- src/hanky/__init__.py
+++ src/hanky/__init__.py
@@ -4,6 +4,7 @@ from .report import CardRecord as CardRecord
 from .report import LoadReport as LoadReport
 from .config import Config as Config
 from .errors import HankyError as HankyError
+from .errors import ConfigError as ConfigError
 from .errors import CollectionInUseError as CollectionInUseError
 from .errors import CollectionNotFoundError as CollectionNotFoundError
 from .errors import ModelNotFoundError as ModelNotFoundError
